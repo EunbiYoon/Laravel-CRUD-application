702e6fcb2ba1785d9b85f3aabe2a4382c562db1f
Eunbi Yoon <yinyinbei0717@gmail.com>
2023-11-07 15:29:13 -0600

    werwer

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -15,6 +15,9 @@
         @endif 
     </div>
     <div>
+        <div>
+            <a href="{{route('product.create')}}">Create a Product</a>
+        </div>
         <table border="1">
             <tr>
                 <th>ID</th>
